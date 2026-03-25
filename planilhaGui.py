4ebcaf486bced03a0093d1ba4ba28f27a575bca7
Ricardo Rodrigues <101021873+ricardorodrigues-60hz@users.noreply.github.com>
2026-03-25 17:37:03 -0300

    CustomTkinter

--- planilhaGui.py
+++ planilhaGui.py
@@ -1,3 +1,4 @@
+import customtkinter as ctk
 import tkinter as tk
 from tkinter import messagebox
 
@@ -6,59 +7,71 @@ class InterfaceAquisicao:
     def __init__(self, root, planilhaService):
         self.root = root
         self.servico = planilhaService
-        self.root.title("Cadastro de Aquisição v1.0")
-        self.root.geometry("450x600")
+        self.root.title("Cadastro de Aquisição v1.1")
+        self.root.geometry("500x850")
+        self.label_titulo = ctk.CTkLabel (
+            self.root,
+            text = "Cadastro de Compra",
+            font = ctk.CTkFont(size = 20, weight = "bold")
+        )
+        self.label_titulo.pack(pady = (20,10))
 
-        # Configuração de Labels e Campos de Entrada
-        self.campos_info = [
-            ("SCD:", "SCD"), ("ID PROTOCOLO:", "ID"), ("OS:", "OS"),
-            ("Data Previsto (dd/mm/aaaa):", "Data Previsto"), ("Item:", "Item"),
-            ("Quantidade:", "Qtde"), ("Valor Unitário:", "Vlr. Unitário"),
+        self.campos_config = [
+            ("SCD:", "SCD"),
+            ("ID PROTOCOLO:", "ID"),
+            ("OS:", "OS"),
+            ("Data Previsto (dd/mm/aaaa):", "Data Previsto"),
+            ("Item:", "Item"),
+            ("Quantidade:", "Qtde"),
+            ("Valor Unitário:", "Vlr. Unitário"),
             ("Fornecedor:", "Fornecedor")
         ]
 
         self.entradas = {}
 
-        # Criar os campos dinamicamente
-        for label_text, chave in self.campos_info:
-            tk.Label(self.root, text=label_text, font=("Times New Roman", 10, "bold")).pack(pady=2)
-            entry = tk.Entry(self.root, width=45)
-            entry.pack(pady=2)
+        #Criação campos
+        for label_text, chave in self.campos_config:
+            label = ctk.CTkLabel(text = label_text, font = ctk.CTkFont(size = 13))
+            label.pack(pady = (10 , 0), anchor = "w", padx = 20)
+
+            entry = ctk.CTkEntry(
+                self.frame_campos,
+                width=400,
+                placeholder_text = f"Digite o {label_text.replace(':', '')}..."
+            )
+            entry.pack(pady=(0, 10), padx=20)
+
             self.entradas[chave] = entry
 
-        # Botão de Ação
-        self.btn_salvar = tk.Button(
-            self.root, text="SALVAR NA PLANILHA", font=("Times New Roman", 12, "bold"),
-            bg="#27ae60", fg="white", command=self.executar_envio, pady=10
+        self.bt_salvar = ctk.CTkButton(
+            self.root,
+            text="SALVAR NA PLANILHA",
+            command=self.executar_envio,
+            font=ctk.CTkFont(size=14, weight="bold"),
+            height=45,
+            fg_color="#27ae60",
+            hover_color="#219150"
         )
-        self.btn_salvar.pack(pady=25)
+        self.bt_salvar.pack(pady=30, padx=20, fill="x")
 
     def executar_envio(self):
-        # Coleta dados dos inputs
+
         dados_finais = {chave: entry.get() for chave, entry in self.entradas.items()}
 
-        if not dados_finais['Item'] or not dados_finais['Data Previsto']:
-            messagebox.showwarning("Atenção", "Preencha ao menos o Item e a Data!")
+        if not dados_finais['SCD'] or not dados_finais['Item']:
+            messagebox.showwarning("Aviso", "Preencha ao menos SCD e Item")
             return
 
         try:
+            self.btn_salvar.configure(state="disabled", text="Enviando...")
+            self.root.update()
+
             self.servico.inserir_registro(dados_finais)
             messagebox.showinfo("Sucesso", "Dados enviados com sucesso!")
             self.limpar_tela()
         except Exception as e:
             messagebox.showerror("Erro Crítico", f"Erro ao conectar: {e}")
 
-    def limpar_tela(self):
-        for entry in self.entradas.values():
-            entry.delete(0, tk.END)
-
-# if __name__ == "__main__":
-#     # Criamos uma classe "Mock" (de mentira) para simular o serviço da planilha
-#     class MockService:
-#         def inserir_registro(self, dados):
-#             print(f"Simulando envio para o Google: {dados}")
-#
-#     root = tk.Tk()
-#     # Passamos o serviço de mentira para a interface
-#     app = InterfaceAquisicao(root, MockService())
-#     root.mainloop()
+    def limpar_campos(self):
+        for entrada in self.entradas.values():
+            entrada.delete(0, 'end')
