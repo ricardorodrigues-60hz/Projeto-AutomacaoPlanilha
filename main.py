f580ea1e02b2e3c04e0365d5d23ec897f85b6080
Ricardo Rodrigues <101021873+ricardorodrigues-60hz@users.noreply.github.com>
2026-03-25 17:39:39 -0300

    Clean

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import tkinter as tk
+import customtkinter as ctk
 from planilhaService import PlanilhaService
 from planilhaGui import InterfaceAquisicao
 
 if __name__ == "__main__":
-    #Inicia o serviço de dados
     servico = PlanilhaService('credentials.json', 'Controle de Compras em Kanban')
-
-    #Inicia a interface e passa o serviço para ela
-    root = tk.Tk()
+    root = ctk.CTk()
     app = InterfaceAquisicao(root, servico)
     root.mainloop()
