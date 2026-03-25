af03bae936ab16365ca553117e913dfa400097c8
Ricardo Rodrigues <101021873+ricardorodrigues-60hz@users.noreply.github.com>
2026-03-25 17:02:12 -0300

    Limpando planilhaService.py

--- planilhaService.py
+++ planilhaService.py
@@ -5,14 +5,13 @@ from datetime import datetime
 
 class PlanilhaService:
     def __init__(self, arquivo_json, nome_planilha):
-        self.arquivo_json = 'credentials.json'
-        self.escopos = [
+
+        self.escopo = [
             "https://www.googleapis.com/auth/spreadsheets",
             "https://www.googleapis.com/auth/drive"
         ]
-        self.SAMPLE_SPREADSHEET_ID = '1cz9WI5ocxhhD7ujOqNCjPhCFXD0X97kFTbP_kE9shXg'
-        self.SAMPLE_RANGE_NAME = 'A2:N100'
-        self.creds = ServiceAccountCredentials.from_json_keyfile_name(filename = arquivo_json, scopes=self.escopos)
+
+        self.creds = ServiceAccountCredentials.from_json_keyfile_name(filename = arquivo_json, scopes=self.escopo)
         self.cliente = gspread.authorize(self.creds)
         self.aba = self.cliente.open('Controle de Compras em Kanban').sheet1
 
